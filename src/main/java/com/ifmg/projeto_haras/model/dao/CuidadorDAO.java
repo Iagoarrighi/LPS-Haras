8e609b05f846525e9f0b3b7a4c7002bc0af2be7f
gfbigogno <gustavobigogno@gmail.com>
2022-12-06 15:19:09 -0300

    sprint 2 - add CuidadorDAO, ProprietarioDAO, VeterinarioDAO

---
+++ src/main/java/com/ifmg/projeto_haras/model/dao/CuidadorDAO.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ifmg.projeto_haras.model.dao;
+
+import com.ifmg.projeto_haras.factory.Database;
+import com.ifmg.projeto_haras.model.Cuidador;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
+import java.util.List;
+
+/**
+ *
+ * @author gusta
+ */
+public class CuidadorDAO implements IDao {
+        private EntityManager entityManager;
+    private Query qry;
+    private String sql;
+    
+    public CuidadorDAO(){
+        entityManager = Database.getInstance().getEntityManager();
+    }
+
+    
+    @Override
+    public void save(Object obj) {
+        Cuidador cuidador = (Cuidador) obj;
+        this.entityManager.getTransaction().begin();
+        if (cuidador != null) {
+            this.entityManager.merge(cuidador);
+        } else {
+            this.entityManager.persist(cuidador);
+        }
+        this.entityManager.getTransaction().commit();
+    }
+
+    
+    @Override
+    public boolean delete(Object obj) {
+        Cuidador cuidador = (Cuidador) obj;
+        this.entityManager.getTransaction().begin();
+        this.entityManager.remove(cuidador);
+        this.entityManager.getTransaction().commit();
+        return true;
+    }
+
+    @Override
+    public Object find(int id) {
+        sql = " SELECT c "
+                + " FROM Cuidador c "
+                + " WHERE id = :id ";
+
+        qry = this.entityManager.createQuery(sql);
+        qry.setParameter("id", id);
+        
+        List lst = qry.getResultList();
+        if (lst.isEmpty()) {
+            return null;
+        } else {
+            return lst.get(0);
+        }
+    }
+    
+    @Override
+    public List<Object> findAll() {
+        sql = " SELECT c "
+                + " FROM Cuidador c ";
+
+        qry = this.entityManager.createQuery(sql);
+        
+        List lst = qry.getResultList();
+        return lst;
+    }
+}
---
+++ src/main/java/com/ifmg/projeto_haras/model/dao/VeterinarioDAO.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ifmg.projeto_haras.model.dao;
+
+import com.ifmg.projeto_haras.factory.Database;
+import com.ifmg.projeto_haras.model.Proprietario;
+import com.ifmg.projeto_haras.model.Veterinario;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
+import java.util.List;
+
+/**
+ *
+ * @author gusta
+ */
+public class VeterinarioDAO implements IDao {
+    private EntityManager entityManager;
+    private Query qry;
+    private String sql;
+    
+    public VeterinarioDAO(){
+        entityManager = Database.getInstance().getEntityManager();
+    }
+
+    
+    @Override
+    public void save(Object obj) {
+        Veterinario veterinario = (Veterinario) obj;
+        this.entityManager.getTransaction().begin();
+        if (veterinario != null) {
+            this.entityManager.merge(veterinario);
+        } else {
+            this.entityManager.persist(veterinario);
+        }
+        this.entityManager.getTransaction().commit();
+    }
+
+    
+    @Override
+    public boolean delete(Object obj) {
+        Veterinario veterinario = (Veterinario) obj;
+        this.entityManager.getTransaction().begin();
+        this.entityManager.remove(veterinario);
+        this.entityManager.getTransaction().commit();
+        return true;
+    }
+
+    @Override
+    public Object find(int id) {
+        sql = " SELECT v "
+                + " FROM Veterinario v "
+                + " WHERE id = :id ";
+
+        qry = this.entityManager.createQuery(sql);
+        qry.setParameter("id", id);
+        
+        List lst = qry.getResultList();
+        if (lst.isEmpty()) {
+            return null;
+        } else {
+            return lst.get(0);
+        }
+    }
+    
+    @Override
+    public List<Object> findAll() {
+        sql = " SELECT v "
+                + " FROM Veterinario v ";
+
+        qry = this.entityManager.createQuery(sql);
+        
+        List lst = qry.getResultList();
+        return lst;
+    }
+}
